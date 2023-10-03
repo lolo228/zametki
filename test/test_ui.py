95ed05f3725113f289d5a1ec257f30822e7bae9b
lolo228 <hojicimo@gmail.com.com>
2023-10-03 20:05:44 +0300

    Co-authored-by: zobus0 <zobus0@users.noreply.github.com>

--- test/test_ui.py
+++ test/test_ui.py
@@ -1,12 +1,24 @@
 import flet as ft
 
+def create_navigation_rails():
+    rail = ft.NavigationRail(
+        selected_index=0,
+        label_type=ft.NavigationRailLabelType.ALL,
+        min_width=100,
+        height=400,
+        leading=ft.FloatingActionButton(icon=ft.icons.ADD, text='Добавить'),
+        destinations=[
+            ft.NavigationRailDestination(icon=ft.icons.NOTE, label='Заметки'),
+            ft.NavigationRailDestination(icon=ft.icons.SEARCH, label='Поиск')
+        ]
+    )
+
+    return rail
+
 def main(page: ft.Page):
-    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    rail = create_navigation_rails()
 
-    for i in range(100):
-        page.controls.append(ft.TextButton(f'Test{i}'))
-    
-    page.scroll = "always"
-    page.update()
+    page.add(ft.Row([rail, ft.VerticalDivider(width=1, color=ft.colors.GREY_50), ft.Column([ft.Text("Test!")])]))
 
-ft.app(target=main)
+if __name__ == '__main__':
+    ft.app(target=main)
